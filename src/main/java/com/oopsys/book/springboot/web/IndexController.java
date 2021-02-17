94b935cdb2b9a8df71b52382ad5b5430256be880
oopsys6 <operasystem6@naver.com>
2021-02-17 19:43:53 +0900

    feat: @LoginUser Annotation 사용
    SessionUser 객체를 직접 생성하지 않고 Annotation을 사용
    
    구글 로그인 서비스

--- src/main/java/com/oopsys/book/springboot/web/IndexController.java
+++ src/main/java/com/oopsys/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.oopsys.book.springboot.web;
 
+import com.oopsys.book.springboot.config.auth.LoginUser;
 import com.oopsys.book.springboot.config.auth.dto.SessionUser;
 import com.oopsys.book.springboot.service.posts.PostsService;
 import com.oopsys.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
